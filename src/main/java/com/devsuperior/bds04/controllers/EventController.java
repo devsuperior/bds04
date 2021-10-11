fd16be2a0222a1541a6d0601d0105d7dfdcb6bab
Armando Giannetti <armando.giannetti@outlook.com>
2021-10-10 22:34:06 -0300

    Final

---
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds04.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+	@Autowired
+	private EventService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+		Page<EventDTO> page = service.findAllPaged(pageable);		
+		return ResponseEntity.ok().body(page);
+	}
+	
+	@PostMapping
+	public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+}
