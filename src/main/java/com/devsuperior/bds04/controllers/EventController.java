7dd3dfbe9ef9c5e54d3e44052ed1c40f0f4dae90
Gabriel Santos de Almeida <1gabrielsantosdealmeida@gmail.com>
2022-09-05 21:03:59 -0300

    Controllers implementation

---
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -0,0 +1,62 @@
+package com.devsuperior.bds04.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+	@Autowired
+	private EventService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+		Page<EventDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> findById(@PathVariable Long id) {
+		EventDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
+		EventDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id,@Valid @RequestBody EventDTO dto) {
+		EventDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
--- src/main/java/com/devsuperior/bds04/services/EventService.java
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -1,5 +1,71 @@
 package com.devsuperior.bds04.services;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+import com.devsuperior.bds04.services.exceptions.DatabaseException;
+import com.devsuperior.bds04.services.exceptions.ResourceNotFoundException;
+
+@Service
 public class EventService {
 
+	@Autowired
+	private EventRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		Page<Event> list = repository.findAll(pageable);
+
+		return list.map(x -> new EventDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public EventDTO findById(Long id) {
+		Optional<Event> obj = repository.findById(id);
+		Event entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found!"));
+		return new EventDTO(entity);
+	}
+
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new EventDTO(entity);
+	}
+
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Event entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID Not Found! ID: " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID Not Found! ID: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation!");
+		}
+	}
+	
 }
